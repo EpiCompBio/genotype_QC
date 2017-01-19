8a98702705e9b46889b24316c2d8795efb8110f9
Antonio <AntonioJBT@users.noreply.github.com>
2017-01-19 16:45:12 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Dummy file, update later
+# For a full version see: https://github.com/CGATOxford/UMI-tools/blob/master/setup.py
+# Or: https://github.com/CGATOxford/cgat/blob/master/setup.py
+
+from setuptools import setup
+
+setup(name='PipelineGenotypeQC',
+      version='0.1',
+#      py_modules=[''],
+)
