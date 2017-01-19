8cd767e318d54002377e9e2580a6ae5a3c26e7ca
Antonio <AntonioJBT@users.noreply.github.com>
2017-01-19 18:22:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-# Dummy file, update later
-# For a full version see: https://github.com/CGATOxford/UMI-tools/blob/master/setup.py
+# https://python-packaging.readthedocs.io/en/latest/minimal.html
+# For a fuller example see: https://github.com/CGATOxford/UMI-tools/blob/master/setup.py
 # Or: https://github.com/CGATOxford/cgat/blob/master/setup.py
 
 from setuptools import setup
 
-setup(name='PipelineGenotypeQC',
+setup(name='pipeline_genotype_QC',
       version='0.1',
-#      py_modules=[''],
-)
+      description='Genotype QC pipeline for thousands of samples and markers follows UKB protocol',
+      url='https://github.com/EpiCompBio/genotype_tools',
+      author='Antonio J Berlanga-Taylor',
+      author_email='a.berlanga at imperial.ac.uk',
+      license='GPL-3.0',
+#      packages=['funniest'],
+      zip_safe=False
+     )
